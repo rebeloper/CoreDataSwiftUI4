416850656b122042549ecc7aef0e4fd37f934fed
rebeloper <therebeloper@gmail.com>
2022-08-25 16:32:48 +0300

    Initial Commit

---
+++ CoreDataSwiftUI4/CoreDataSwiftUI4App.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataSwiftUI4App.swift
+//  CoreDataSwiftUI4
+//
+//  Created by Alex Nagy on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataSwiftUI4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
